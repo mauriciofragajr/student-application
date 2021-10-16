00651467442e7bf8c565356014888d346e571afa
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2021-10-16 10:27:57 -0300

    Add graphql connection config

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: `${process.env.REACT_APP_GRAPHQL_URI}/graphql`,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
