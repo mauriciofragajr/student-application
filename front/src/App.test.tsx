8604c59f74fd35fc1379147ea6f79a5eef89742b
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2021-10-16 07:11:51 -0300

    Add base react project using typescript

---
+++ front/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Student App text', () => {
+  render(<App />);
+  const textElement = screen.getByText(/Student App/i);
+  expect(textElement).toBeInTheDocument();
+});
---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Student App
+    </div>
+  );
+}
+
+export default App;
