c651c08b17925fb5f22c7eda355e29946970feb8
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2021-10-17 11:33:48 -0300

    Add feature get student hook using filter

--- front/src/components/student-list/StudentList.tsx
+++ front/src/components/student-list/StudentList.tsx
@@ -1,38 +1,96 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { TableContainer, Paper, Table, TableHead, TableCell, TableBody, TableRow, TextField, Box } from '@mui/material';
 
 import { Student } from "../../domain/interfaces/Student";
-import { useGetStudents } from "../../hooks/students/useGetStudents";
-import { TableContainer, Paper, Table, TableHead, TableCell, TableBody, TableRow } from '@mui/material';
+import { useGetStudentsFilter } from '../../hooks/students/useGetStudentsFilter';
+import { useForm } from 'react-hook-form';
 
 const StudentList: React.FC = () => {
-  const { loading, data } = useGetStudents()
+  const [students, { loading, data }] = useGetStudentsFilter();
+  const { register, handleSubmit } = useForm<Student>();
 
   const rows: Student[] = data?.students.nodes || []
 
+  const searchStudents = async (student: Student) => {
+    try {
+      await students({
+        variables: {
+          paging: { offset: 0, limit: 10 },
+          filter: {
+            cpf: {
+              iLike: `%${student.cpf}%`
+            },
+            name: {
+              iLike: `%${student.name}%`
+            },
+            email: {
+              iLike: `%${student.email}%`
+            }
+          }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    searchStudents({ cpf: '', name: '', email: '' });
+  }, []);
+
+  const onSubmit = handleSubmit(async (student: Student) => {
+    try {
+      await searchStudents(student);
+    } catch (e) {
+    }
+  });
+
   return (
-    <div>
-      {loading? (<p>Carregando...</p>) : (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="caption table">
-            <caption>Tabela de estudantes</caption>
-            <TableHead>
-              <TableRow>
-                <TableCell>CPF</TableCell>
-                <TableCell>Nome</TableCell>
-                <TableCell>E-mail</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.cpf}>
-                  <TableCell>{row.cpf}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.email}</TableCell>
+    <div style={{ margin: 20 }}>
+      {loading ? (<p>Carregando...</p>) : (
+        <div>
+          <form onSubmit={onSubmit}>
+            <Box sx={{ display: 'flex' }} justifyContent="center">
+              <Box sx={{ mx: 0.5 }}>
+                <TextField type="text" {...register("cpf")} label="CPF" variant="outlined" />
+              </Box>
+              <Box sx={{ mx: 0.5 }}>
+                <TextField type="text" {...register("name")} label="Nome" variant="outlined" />
+              </Box>
+              <Box sx={{ mx: 0.5 }}>
+                <TextField type="text" {...register("email")} label="E-mail" variant="outlined" />
+              </Box>
+              <Box sx={{ mx: 0.5 }}>
+                <TextField type="submit" value="Pesquisar" />
+              </Box>
+              <Box sx={{ mx: 0.5 }}>
+                <TextField type="reset" value="Limpar" />
+              </Box>
+            </Box>
+          </form>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="caption table">
+              <caption>Tabela de estudantes</caption>
+              <TableHead>
+                <TableRow>
+                  <TableCell>CPF</TableCell>
+                  <TableCell>Nome</TableCell>
+                  <TableCell>E-mail</TableCell>
                 </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.cpf}>
+                    <TableCell>{row.cpf}</TableCell>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                  </TableRow>
                 ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       )}
     </div>
   )
---
+++ front/src/hooks/students/useGetStudentsFilter.ts
@@ -0,0 +1,37 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import { Student } from "../../domain/interfaces/Student";
+
+interface OffsetPageInfo {
+  hasNextPage?: boolean;
+  hasPreviousPage?: boolean;
+}
+
+interface StudentsData {
+  students: {
+    pageInfo: OffsetPageInfo
+    nodes: Student[]
+    totalCount: number
+  }
+}
+
+const GET_STUDENTS_FILTER = gql`
+    query students($filter: StudentFilter!, $paging: OffsetPaging!) {
+      students(filter: $filter, paging: $paging) {
+        nodes {
+          cpf
+          name
+          email
+        },
+        totalCount,
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
+      }
+    }
+  `
+
+export const useGetStudentsFilter = (): any | undefined => {
+  const [students, { loading, data }] = useLazyQuery<StudentsData>(GET_STUDENTS_FILTER)
+  return [students, { loading, data }]
+}
